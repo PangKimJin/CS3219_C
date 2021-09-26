133862f23a1c158e06a3a9b1358d698236275e96
PangKimJin <pangkimjin1@gmail.com>
2021-09-26 13:13:10 +0800

    added heroku postbuild

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+const publicPath = path.join(__dirname, 'web/src');
 
-app.use(express.json());
+app.use(express.static(publicPath));
 app.use(express.urlencoded({extended: true})); 
-app.use(express.json());
+// app.use(express.json());
 
 let apiRoutes = require("./api-routes");
 
 app.get('/', (req, res) => {
+    res.sendFile(path.join(publicPath, 'App.js'));
+});
+
+app.get('/hello', (req, res) => {
     res.send('hello world');
 });
 
